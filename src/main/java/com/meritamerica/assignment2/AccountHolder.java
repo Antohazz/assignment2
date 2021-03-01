39dae86b52eeb3d9a1e592f92b7d2b3085bd0e47
Anton Ivanov <antoniv.eb@gmail.com>
2021-03-01 11:30:46 -0500

    ding dong

--- src/main/java/com/meritamerica/assignment2/AccountHolder.java
+++ src/main/java/com/meritamerica/assignment2/AccountHolder.java
@@ -2,30 +2,121 @@ package com.meritamerica.assignment2;
 
 import java.util.Arrays;
 
+import com.meritamerica.assignment1.CheckingAccount;
+import com.meritamerica.assignment1.SavingsAccount;
+
+/* AccountHolder class for MeritAmericaBankApp.
+ * Allows to create new account account.
+ * Provides getter and setter methods to access account's properties.
+ */
+
+
 public class AccountHolder {
-}
+	
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private String ssn;
+	private CheckingAccount checkingAccount;
+	private SavingsAccount savingsAccount;
+
+	
+	//Constructors
+	
+	public AccountHolder() {
+
+	}
+	
+	public AccountHolder (String firstName, String middleName, String lastName, String ssn) {
+
+		this.firstName = firstName;
+		this.middleName = middleName;
+		this.lastName = lastName;
+		this.ssn = ssn;
+		
+	}
+		
+	
+	//AccountHolder primary GETTERS and SETTERS
+	
+	//First name setter & getter
+	
+	protected void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	protected String getFirstName() {
+		return firstName; 
+	}
+	
+	
+	//Middle name setter & getter
+	
+	protected void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	protected String getMiddleName() {
+		return middleName; 
+	}
+	
+	
+	//Last name setter & getter
+	
+	protected void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	protected String getLastName() {
+		return lastName; 
+	}
+	
+	//SSN setter & getter
+	
+	protected void setSSN(String ssn) {
+		this.ssn = ssn;
+	}
+
+	protected String getSSN() {
+		return ssn; 
+	}
+	
+
+	//ACCOUNTS creation and access
+	
+	//CHECKING
+	
+	private CheckingAccount addCheckingAccount(double openingBalance) {
+		
+	}
+	
+	private CheckingAccount addCheckingAccount(CheckingAccount checkingAccount) {
+		
+	}
+	
+	protected CheckingAccount[] getCheckingAccounts() {
+		
+	}
+	protected int getNumberOfCheckingAccounts() {
+		
+	}
+	protected double getCheckingBalance() {
+		
+	}
+
+	
+}//class
 
 
-//
-//AccountHolder(String firstName, String middleName, String lastName, String ssn)
-//String getFirstName()
-//void setFirstName()
-//String getMiddleName()
-//void setMiddleName()
-//String getLastName()
-//void setLastName()
-//String getSSN()
-//void setSSN()
-//CheckingAccount addCheckingAccount(double openingBalance)
-//CheckingAccount addCheckingAccount(CheckingAccount checkingAccount)
-//CheckingAccount[] getCheckingAccounts()
-//int getNumberOfCheckingAccounts()
-//double getCheckingBalance()
+
+
+
 //SavingsAccount addSavingsAccount(double openingBalance)
 //SavingsAccount addSavingsAccount(SavingsAccount savingsAccount)
 //SavingsAccount[] getSavingsAccounts()
 //int getNumberOfSavingsAccounts()
 //double getSavingsBalance()
+
 //CDAccount addCDAccount(CDOffering offering, double openingBalance)
 //CDAccount addCDAccount(CDAccount cdAccount)
 //CDAccount[] getCDAccounts()
