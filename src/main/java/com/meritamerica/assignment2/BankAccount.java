8072d5fca413d392a361e10ba5dae5168a16e089
Anton Ivanov <antoniv.eb@gmail.com>
2021-03-02 23:36:41 -0500

    fts

--- src/main/java/com/meritamerica/assignment2/BankAccount.java
+++ src/main/java/com/meritamerica/assignment2/BankAccount.java
@@ -1,7 +1,62 @@
 package com.meritamerica.assignment2;
 
 public class BankAccount {
-
+	
+	private double interestRate;
+	private long accountNumber;
+	private double balance;
+	
+	public BankAccount() {
+		
+	}
+	
+	public BankAccount(double balance, double interestRate) {
+		this.balance = balance;
+		this.interestRate = interestRate;
+	}
+	
+	public BankAccount(long accountNumber, double balance, double interestRate) {
+		this.accountNumber = accountNumber;
+		this.balance = balance;
+		this.interestRate = interestRate;
+	}
+	
+	public long getAccountNumber() {
+		return accountNumber;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	
+	public double getInterestRate() {
+		return interestRate;
+	}
+	
+	public boolean withdraw(double amount) {
+		if (amount > 0 && amount <= balance) {
+			balance -=amount;
+			return true;
+		}else {
+			System.out.println("Incorrect amount or exceeding balance.");
+			return false;
+			}
+	}
+	
+	public boolean deposit (double amount) {
+		if(amount > 0) {
+			balance +=amount;
+			return true;
+		}else {
+			System.out.println("Can't be zero or negative.");
+			return false;
+		}
+	}
+	
+	public double futureValue(int years) {
+		double futureBalance = balance * Math.pow(1 + getInterestRate(), years);
+		return futureBalance;
+	}
 
 	
 	
--- src/main/java/com/meritamerica/assignment2/SavingsAccount.java
+++ src/main/java/com/meritamerica/assignment2/SavingsAccount.java
@@ -9,57 +9,23 @@ package com.meritamerica.assignment2;
 public class SavingsAccount extends BankAccount{
 	
 	private final double INTEREST_RATE = 0.01; // Interest rate
-	private double balance = 0.0;	// Current balance
+	private double balance;	// Current balance
 	
 	//Sets opening balance
 	SavingsAccount(double openingBalance){
 		this.balance = openingBalance;
 	}
-	
-	//Balance getter
-	protected double getBalance() {
-		return balance;
-	}
-	
+
 	//Interest getter
-	protected double getInterestRate() {
+	public double getInterestRate() {
 		return INTEREST_RATE;
 	}
-	
-	//Withdraw method. Overdraft not allowed.
-	protected boolean withdraw(double amount) {
-		if (amount > 0 && amount <= balance) {
-			balance -=amount;
-			return true;
-		}else {
-			System.out.println("Incorrect amount or exceeding balance.");
-			return false;
-		}
-		
-	}
-	
-	//Deposit method. Not allowed 0 & negative deposit.
-	protected boolean deposit(double amount) {
-		if(amount > 0) {
-			balance +=amount;
-			return true;
-		}else {
-			System.out.println("Can't be zero or negative.");
-			return false;
-		}
-		
-	}
-	
-	// Outputs the future value of the account balance based on the interest/years.
-	protected double futureValue(int years) {
-		double futureBalance = balance * Math.pow(1 + getInterestRate(), years);
-		return futureBalance;
-	}
+
 	
 	
 	// Outputs account info
 	public String toString() {
-		String saveAccInfo = "Savings Account Balance: $" + balance + "/n"+
+		String saveAccInfo = "Savings Account Balance: $" + getBalance() + "/n"+
 				"Savings Account Interest Rate: " + getInterestRate() + "/n"+
 				"Savings Account Balance in 3 years: $" + futureValue(3);
 		return saveAccInfo;
