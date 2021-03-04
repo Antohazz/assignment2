f81d06abe4e53a4a67df6264f786dfaf61842794
Anton Ivanov <antoniv.eb@gmail.com>
2021-03-04 16:39:36 -0500

    na

--- src/main/java/com/meritamerica/assignment2/MeritAmericaBankApp.java
+++ src/main/java/com/meritamerica/assignment2/MeritAmericaBankApp.java
@@ -7,19 +7,34 @@ public class MeritAmericaBankApp {
 		
     	CDOffering[] CDOfferings = new CDOffering[5];
     	
-    	CDOfferings[0] = new CDOffering(1,1.8/100);
-    	CDOfferings[1] = new CDOffering(2,1.9/100);
-    	CDOfferings[2] = new CDOffering(3,2.0/100);
-    	CDOfferings[3] = new CDOffering(5,2.5/100);
-    	CDOfferings[4] = new CDOffering(10,2.2/100);
+    	CDOfferings[0] = new CDOffering(1,0.018);
+    	CDOfferings[1] = new CDOffering(2,0.019);
+    	CDOfferings[2] = new CDOffering(3,0.02);
+    	CDOfferings[3] = new CDOffering(5,0.025);
+    	CDOfferings[4] = new CDOffering(10,0.022);
     	
     	MeritBank.setCDOfferings(CDOfferings);
 		
+    	System.out.println("CD offerings");
+        for (int i  = 0; i < CDOfferings.length; i++) {
+        	System.out.println(CDOfferings[i].getInterestRate());
+        	System.out.println(CDOfferings[i].getTerm());
+        }
+    	
 		/* Instantiates an account holder
 		with a checking balance of $100 and a savings balance of $1000*/
 		AccountHolder ah1 = new AccountHolder("John", "James", "Doe", "123-45-6789");
 		
+
+			
+		//Add the best CD offering as a CD account on ah1
+		MeritBank.getBestCDOffering(ah1.getCombinedBalance());
 		
+		//Add ah1 to Merit Bank’s list of account holders
+		MeritBank.addAccountHolder(ah1);
+		
+		
+		System.out.println("Account 1");
 		ah1.addCheckingAccount(1000);
 		ah1.addSavingsAccount(10000);
 		ah1.addCheckingAccount(5000);
@@ -28,23 +43,13 @@ public class MeritAmericaBankApp {
 		ah1.addSavingsAccount(500000);
 		ah1.addCheckingAccount(5000);
 		ah1.addSavingsAccount(50000);
-		
-        for (int i  = 0; i < CDOfferings.length; i++) {
-        	System.out.println(CDOfferings[i].getInterestRate());
-        	System.out.println(CDOfferings[i].getTerm());
-        }
-		
+				
 		System.out.println(ah1.getNumberOfCheckingAccounts());
 		System.out.println(ah1.getNumberOfSavingsAccounts());
 		System.out.println(ah1.getCheckingBalance());
 		System.out.println(ah1.getSavingsBalance());
 		System.out.println(ah1.getCombinedBalance());
-			
-		//Add the best CD offering as a CD account on ah1
-		MeritBank.getBestCDOffering(ah1.getCombinedBalance());
 		
-		//Add ah1 to Merit Bank’s list of account holders
-		MeritBank.addAccountHolder(ah1);
 		
 		
 		
@@ -55,6 +60,7 @@ public class MeritAmericaBankApp {
 		ah2.addCheckingAccount(1000);
 		ah2.addSavingsAccount(10000);
 		
+
 		//Add the second best CD offering as a CD account on ah2
 		MeritBank.getSecondBestCDOffering(ah2.getCombinedBalance());
 		
@@ -65,6 +71,16 @@ public class MeritAmericaBankApp {
 		MeritBank.clearCDOfferings();
 		
 		
+		System.out.println("Account 2");
+		System.out.println(ah2.getNumberOfCheckingAccounts());
+		System.out.println(ah2.getNumberOfSavingsAccounts());
+		System.out.println(ah2.getCheckingBalance());
+		System.out.println(ah2.getSavingsBalance());
+		System.out.println(ah2.getCombinedBalance());
+		
+		
+		
+		
 		//Instantiate a new AccountHolder (ah3)
 		AccountHolder ah3 = new AccountHolder("Dick", "Herbert", "Herring", "555-55-5555");
 		
@@ -73,13 +89,25 @@ public class MeritAmericaBankApp {
 		
 		//Confirm a CD account was not created on ah3
 		ah3.getCDAccounts();
+		
 		//Add a checking account with an opening balance of $1,000 as well as a savings account with an opening balance of $10,000 to ah3
+		ah3.addCheckingAccount(1000);
+		ah3.addSavingsAccount(10000);
 		
 		//Add ah3 to Merit Bank’s list of account holders
 		MeritBank.addAccountHolder(ah3);
 		
+		System.out.println("Account 3");
+		System.out.println(ah3.getNumberOfCheckingAccounts());
+		System.out.println(ah3.getNumberOfSavingsAccounts());
+		System.out.println(ah3.getCheckingBalance());
+		System.out.println(ah3.getSavingsBalance());
+		System.out.println(ah3.getCombinedBalance());
+		
+		
 		//Get the total balance of all accounts held by Merit Bank’s account holders
 		MeritBank.totalBalances();
-		
+		System.out.println("Total assets");
+		System.out.println(MeritBank.totalBalances());
 	}
 }
